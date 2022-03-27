b3a55544927f64ad5f1f167b775468518a1bec47
DemonWayne <DimasPro228@i.ua>
2022-03-27 17:51:02 +0300

    Initialize commit

---
+++ src/commands/General/ping.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { Command } = require('@sapphire/framework');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      description: 'Пинг бота',
+    });
+  }
+
+  async messageRun(message) {
+    const msg = await message.channel.send({
+      embeds: [new MessageEmbed().setColor('DARK_BUT_NOT_BLACK').setDescription('**Вычисление...**')],
+    });
+
+    const latency = Math.ceil(msg.createdTimestamp - message.createdTimestamp);
+    const color = latency < 100 ? 0x00ff99 : latency < 250 ? 0x00cc66 : latency < 500 ? 0xff9900 : 0xcc0000;
+
+    return msg.edit({
+      embeds: [
+        new MessageEmbed()
+          .setColor(color)
+          .setDescription(
+            `**Пинг бота: \`${latency}ms\`\nПинг API: \`${Math.round(this.container.client.ws.ping)}ms\`**`,
+          ),
+      ],
+    });
+  }
+};
---
+++ src/listeners/ready.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { Listener } = require('@sapphire/framework');
+
+module.exports = class extends Listener {
+  run(client) {
+    const { tag } = client.user;
+    this.container.logger.info(`Бот авторизован как ${tag}`);
+  }
+};
